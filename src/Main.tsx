dfbbe9801be661178b5fe143487518293f69295a
Andrew <andrewlmanley@gmail.com>
2020-02-01 16:34:40 -0500

    Added extension storage methods

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,22 +3,14 @@ import { hot } from 'react-hot-loader';
 
 import { Store } from './redux/Store';
 
-import { testString } from '@browser-extension/utility-storage';
-
 class Main extends React.Component<Store> {
-  onClickButton(): void {
-    this.props.TestAction('NEW CONTENT');
-  }
+  componentDidMount(): void {}
 
   render(): JSX.Element {
     return (
       <div>
         <p>Howdy Yall!</p>
-        <p>{testString}</p>
         <p>{this.props.testObject.name}</p>
-        <button className="button" onClick={(): void => this.onClickButton()}>
-          Click this
-        </button>
       </div>
     );
   }
