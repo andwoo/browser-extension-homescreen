4ca1809d6d92e0950aa3e80233ba1cafcb88b6f0
Andrew Manley <andrewlmanley@gmail.com>
2020-02-01 09:59:37 -0500

    Added gitsubmodule extension utility storage Added yarn workspace

---
+++ REAMDME.md
@@ -0,0 +1,5 @@
+# Submodules
+To install submodules run the command from the root
+```
+git submodule update --init --recursive
+```
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { hot } from 'react-hot-loader';
 
 import { Store } from './redux/Store';
 
+import { testString } from '@browser-extension/utility-storage';
+
 class App extends React.Component<Store> {
   onClickButton(): void {
     this.props.TestAction('NEW CONTENT');
@@ -12,6 +14,7 @@ class App extends React.Component<Store> {
     return (
       <div>
         <p>Howdy Yall!</p>
+        <p>{testString}</p>
         <p>{this.props.testObject.name}</p>
         <button className="button" onClick={(): void => this.onClickButton()}>
           Click this
