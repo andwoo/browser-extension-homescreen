bd72f5245c21474962f9e7a2801378a976a2af0e
Andrew <andrewlmanley@gmail.com>
2020-02-03 20:54:33 -0500

    Added the header component and rough content layout

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,43 +1,41 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
-
 import { Store } from './redux/Store';
+import Header from './components/main/Header';
 
 class Main extends React.Component<Store> {
   componentDidMount(): void {}
 
   render(): JSX.Element {
     return (
-      <div className="columns">
-        <div className="column">
-          {/* Links Start */}
-          <div className="card icons">Links</div>
-          {/* Links End */}
+      <React.Fragment>
+        {/* Links Start */}
+        <Header />
+        {/* Links End */}
 
-          <div className="columns">
-            {/* Content Tiles Start */}
-            <div className="column">
-              <div className="card tile">twitch</div>
-            </div>
-            <div className="column">
-              <div className="card tile">csgo</div>
-            </div>
-            <div className="column">
-              <div className="card tile">overwatch</div>
-            </div>
-            <div className="column">
-              <div className="card tile">reddit1</div>
-            </div>
-            <div className="column">
-              <div className="card tile">reddit2</div>
-            </div>
-            <div className="column">
-              <div className="card tile">reddit3</div>
-            </div>
-            {/* Content Tiles End */}
+        <div className="columns is-variable is-1 tile--container">
+          {/* Content Tiles Start */}
+          <div className="column">
+            <div className="card tile">twitch</div>
           </div>
+          <div className="column">
+            <div className="card tile">csgo</div>
+          </div>
+          <div className="column">
+            <div className="card tile">overwatch</div>
+          </div>
+          <div className="column">
+            <div className="card tile">reddit1</div>
+          </div>
+          <div className="column">
+            <div className="card tile">reddit2</div>
+          </div>
+          <div className="column">
+            <div className="card tile">reddit3</div>
+          </div>
+          {/* Content Tiles End */}
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/main/Header.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+
+interface NavbarLink {
+  class: string;
+  href: string;
+}
+
+const navbarLinks: Array<NavbarLink> = [
+  {
+    class: 'fad fa-envelope',
+    href: 'https://mail.google.com/',
+  },
+  {
+    class: 'fab fa-google-drive',
+    href: 'https://drive.google.com/',
+  },
+  {
+    class: 'fad fa-calendar',
+    href: 'https://calendar.google.com/',
+  },
+  {
+    class: 'fab fa-youtube',
+    href: 'https://www.youtube.com/',
+  },
+  {
+    class: 'fab fa-twitch',
+    href: 'https://www.twitch.tv/',
+  },
+  {
+    class: 'fad fa-film',
+    href: 'https://www.netflix.com/browse',
+  },
+  {
+    class: 'fad fa-toilet-paper-alt',
+    href: 'https://www.crunchyroll.com/',
+  },
+  {
+    class: 'fab fa-soundcloud',
+    href: 'https://soundcloud.com/discover',
+  },
+  {
+    class: 'fab fa-reddit',
+    href: 'https://www.reddit.com/',
+  },
+  {
+    class: 'fab fa-twitter',
+    href: 'https://twitter.com/home',
+  },
+  {
+    class: 'fad fa-code-branch',
+    href: 'https://github.com/',
+  },
+  {
+    class: 'fab fa-amazon',
+    href: 'https://www.amazon.ca/',
+  },
+];
+
+export default class Header extends React.Component {
+  render(): JSX.Element {
+    return (
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <div className="navbar-item">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+          </div>
+        </div>
+        {navbarLinks.map((link: NavbarLink, index: number) => {
+          return (
+            <a className="navbar-item" href={link.href} key={index}>
+              <i className={link.class}></i>
+            </a>
+          );
+        })}
+      </nav>
+    );
+  }
+}
