f26a3219e233a019cbd9479e2058cc4cc59435b3
Andrew M <andrewlmanley@gmail.com>
2021-05-22 17:09:42 -0400

    Added re-order logic

--- app/components/content/index.ts
+++ app/components/content/index.ts
@@ -18,13 +18,41 @@ export default class ContentComponent extends Component<{}> {
   }
 
   @action addBlock(type: BlockType): void {
+    const order: number = this.blockService.blocks
+      .map((block) => block.order)
+      .reduce(
+        (acc: number, current: number): number => Math.max(current + 1, acc),
+        0
+      );
     this.blockService.addBlock(
       new Block({
         type,
-        order: 0,
+        order,
       })
     );
     this.blockService.save(getOwner(this));
+  }
+
+  @action moveBlockLeft(block: Block): void {
+    const orderedBlocks = this.blockService.blocks.sortBy('order');
+    const previousBlock = orderedBlocks[orderedBlocks.indexOf(block) - 1];
+    if (previousBlock) {
+      const buffer = block.order;
+      block.order = previousBlock.order;
+      previousBlock.order = buffer;
+      this.blockService.save(getOwner(this));
+    }
+  }
+
+  @action moveBlockRight(block: Block): void {
+    const orderedBlocks = this.blockService.blocks.sortBy('order');
+    const nextBlock = orderedBlocks[orderedBlocks.indexOf(block) + 1];
+    if (nextBlock) {
+      const buffer = block.order;
+      block.order = nextBlock.order;
+      nextBlock.order = buffer;
+      this.blockService.save(getOwner(this));
+    }
   }
 
   @action removeBlock(block: Block): void {
