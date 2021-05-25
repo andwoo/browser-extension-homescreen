0c0c9c7adc053b15cf05984d96485c8f9e921b5d
Andrew M <andrewlmanley@gmail.com>
2021-05-25 10:44:13 -0400

    Added launcher block

--- app/components/blocks/view/launcher/index.ts
+++ app/components/blocks/view/launcher/index.ts
@@ -3,6 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { ViewBlockArgs } from '../../../content';
 import Launcher from '../../../../models/launcher';
+import { isEmpty } from '@ember/utils';
 
 export default class ViewLauncherComponent extends Component<ViewBlockArgs> {
   @tracked launchers: Launcher[] = [];
@@ -12,5 +13,11 @@ export default class ViewLauncherComponent extends Component<ViewBlockArgs> {
     this.launchers = (parsed?.launchers ?? []).map((json: string) =>
       Launcher.create(json)
     );
+  }
+
+  @action onLaunchUrl(launcher: Launcher): void {
+    if (!isEmpty(launcher.url)) {
+      window.open(launcher.url, '_blank');
+    }
   }
 }
