a07d8da3dabe9bbe944e4cf8a4aebabb3a3f37b7
Andrew M <andrewlmanley@gmail.com>
2021-05-25 12:05:16 -0400

    Added reddit block

--- app/components/blocks/view/reddit/index.ts
+++ app/components/blocks/view/reddit/index.ts
@@ -2,12 +2,79 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { ViewBlockArgs } from '../../../content';
 import { action } from '@ember/object';
+import axios from 'axios';
+import { taskFor, perform } from 'ember-concurrency-ts';
+import { restartableTask, TaskGenerator } from 'ember-concurrency';
+import { isEmpty } from '@ember/utils';
+
+interface RedditPostResponse {
+  data: {
+    title: string;
+    url: string;
+    score: string;
+    thumbnail: string;
+  };
+}
+
+interface RedditPost {
+  title: string;
+  thumbnail: string;
+  postUrl: string;
+  commentsUrl: string;
+  upVotes: number;
+}
+
+const parser: DOMParser = new DOMParser();
+const decodeString = (value: string) =>
+  parser.parseFromString('<!doctype html><body>' + value, 'text/html').body
+    .textContent;
 
 export default class ViewRedditComponent extends Component<ViewBlockArgs> {
   @tracked reddit: string = '';
 
+  get posts(): RedditPost[] {
+    return taskFor(this.fetchPosts).lastSuccessful?.value ?? [];
+  }
+
+  get isLoading(): boolean {
+    return taskFor(this.fetchPosts).isRunning;
+  }
+
+  get isFailure(): boolean {
+    return !!taskFor(this.fetchPosts).lastErrored;
+  }
+
   @action initialize(): void {
     const parsed = JSON.parse(this.args.item.payload ?? '{}');
     this.reddit = parsed.reddit ?? '';
+    perform(this.fetchPosts, this.reddit);
+  }
+
+  @restartableTask *fetchPosts(reddit: string): TaskGenerator<RedditPost[]> {
+    const response = yield axios.get(`https://reddit.com/r/${reddit}.json`);
+    return response.data.data.children.map((post: RedditPostResponse) => {
+      let thumbnail: string = post.data.thumbnail;
+      if (
+        !thumbnail ||
+        thumbnail === 'self' ||
+        thumbnail === 'spoiler' ||
+        thumbnail === 'nsfw'
+      ) {
+        thumbnail = '/dist/anchor.svg';
+      }
+      return {
+        title: decodeString(post.data.title),
+        thumbnail: thumbnail,
+        postUrl: post.data.url,
+        commentsUrl: `https://reddit.com${post.data.permalink}`,
+        upVotes: post.data.score,
+      };
+    });
+  }
+
+  @action onLaunchCommentsUrl(post: RedditPost): void {
+    if (!isEmpty(post.commentsUrl)) {
+      window.open(post.commentsUrl, '_blank');
+    }
   }
 }
